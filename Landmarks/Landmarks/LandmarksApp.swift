abe138dc84ec1bf7ecfb525ad185b3678757a1f4
alexey-z-m <alexey.panda@gmail.com>
2022-08-29 12:10:13 +0300

    Initial commit

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Panda on 29.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
